2066f2bf60637a0830c03e380b4927f2cc88fd32
TypeLuis <typeluis@gmail.com>
2025-12-11 15:28:53 +0000

    learning rust

---
+++ hello.rs
@@ -0,0 +1,16 @@
+fn main(){
+    println!("Hello, 🦀!")
+}
+
+/*
+rustc hello.rs -> compiles the program to file called hello
+
+./hello -> runs the program, any changes made you're going to have to compile again with rustc projectname
+
+cargo new helloProject -> Creates a folder with the file from current folder with the nessecary package packages 
+
+
+cd helloProject -> cargo run -> runs the project from the src, also creates a target folder and cargo.lock
+
+when creating cargo new project, it already inits a git so you can remove it to add everything with rm -rf helloProject/.git
+*/
